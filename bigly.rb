a9638953b7db484b1bfc783518abb6adddaffdaa
Ray Arnold <wesleyday@hotmail.com>
2016-11-19 16:20:45 -0600

    spread long messages into multiple replies

--- bigly.rb
+++ bigly.rb
@@ -7,32 +7,42 @@ puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
 bot.command :bigly do |_event, *args|
-  msg = ''
+  words = []
   args.each do |arg|
-    if (arg.count ":").to_i > 1
-    	msg << "#{arg[arg.index(':')..arg.rindex(':')]}   "
-    else
-	  	arg.downcase!
-	    arg.split("").each do |a|
-	      if a =~ /[a-z]/
-	       msg << ":regional_indicator_#{a}:"
-	     	elsif a =~ /[0-9]/
-	       msg << ":#{a.to_i.humanize}:"
-	      elsif a == "#"
-         msg << ":hash:" 
-	      elsif a == "*"
-         msg << ":asterisk:"
-	     	else
-	       msg << "#{a}"
-	     	end
-	    end
-	    msg << '   '
+    word = ''
+    unless (arg.count ":").to_i > 1
+      arg.downcase!
+      arg.split("").each do |a|
+        if a =~ /[a-z]/
+          word << ":regional_indicator_#{a}:"
+        elsif a =~ /[0-9]/
+          word << ":#{a.to_i.humanize}:"
+        elsif a == "#"
+          word << ":hash:" 
+        elsif a == "*"
+          word << ":asterisk:"
+        else
+          word << "#{a}"
+        end
+      end
     end
+    words << word
   end
 
-  unless msg[0] == '/'
-  	msg = msg[0..2000]
-    msg[0..msg.rindex(':')]
+  msg = ''
+  words.each do |word|
+    if msg.length + word.length + 3 > 1999
+      unless msg[0] == '/'
+        _event.respond msg
+        sleep(1)
+      end
+      msg = "#{word}   "
+    else
+      msg << "#{word}   "
+    end 
+  end
+  unless msg[0] == '/' || msg == ''
+    _event.respond msg
   end
 end
 
